e54053ff3fec07fd90260bb0f556a86d6de89810
Radchenko Danylo <radchenko.danila@gmail.com>
2023-01-26 01:25:13 +0200

    лишилося доробити слайдер і адаптивний бургер меню

--- Week1/JavaScript/header_drop-down.js
+++ Week1/JavaScript/header_drop-down.js
@@ -1,5 +1,4 @@
 let submenu = document.querySelector('.header__drop__down__menu');
-
 let openButton = document.querySelector('.header__sub__menu');
 let closeButton = document.querySelector('.drop__down__menu__close__button');
 
@@ -9,6 +8,20 @@ openButton.addEventListener('click', () => {
 });
 closeButton.addEventListener('click', () => {
    submenu.classList.add('header__drop__down__menu-close');
+});
+
+console.log("PAMAGITIIII");
+
+let dropdown = document.querySelectorAll('.drop-down');
+
+dropdown.forEach(item => {
+   item.addEventListener('click', () => {
+      if (item.classList.contains('active')) {
+         item.classList.remove('active');
+      } else {
+         item.classList.toggle('active');
+      }
+   });
 });
 
 
