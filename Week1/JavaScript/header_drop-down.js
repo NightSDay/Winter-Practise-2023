3f3dafe1a624b9d8830c0f62408a8efc3f8a3f20
Radchenko Danylo <radchenko.danila@gmail.com>
2023-02-04 02:09:08 +0200

    все що встиг

--- Week1/JavaScript/header_drop-down.js
+++ Week1/JavaScript/header_drop-down.js
@@ -1,4 +1,5 @@
 let submenu = document.querySelector('.header__drop__down__menu');
+let subMenuMobile = document.querySelector('.burger__menu__mobile');
 let openButton = document.querySelector('.header__sub__menu');
 let closeButton = document.querySelector('.drop__down__menu__close__button');
 
@@ -24,5 +25,17 @@ dropdown.forEach(item => {
    });
 });
 
+console.log("PAMAGITIIII");
+
+openButton.addEventListener('click', () => {
+   const screenWidth = document.documentElement.clientWidth;
+
+   if (screenWidth > 720) {
+      submenu.classList.remove('header__drop__down__menu-close');
+   } else {
+      subMenuMobile.classList.toggle('header__drop__down__menu-close');
+      submenu.classList.toggle('header__drop__down__menu-close');
+   }
+});
 
 console.log("PAMAGITIIII");
